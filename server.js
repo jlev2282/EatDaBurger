c9cf6043005845538c551f12f7057ac125969e1a
John <jlev2282@gmail.com>
2017-07-29 23:02:27 -0400

    adding controllers

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
 //dependencies
 var express = require("express");
-var methodoveride = require("method-overide");
-var bodyparser = require("body-parser");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var port = 2207;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(port);
