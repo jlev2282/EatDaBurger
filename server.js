54094630dfbfe76fd3951e42ec638539743fd9c9
John <jlev2282@gmail.com>
2017-09-10 09:42:36 -0400

    env process for heroku added in server.js

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-// app.listen(process.env.PORT || port);
-app.listen(port);
+app.listen(process.env.PORT || port);
+// app.listen(port);
 console.log("Congratulations Coder! I'm listening on port "+port);
