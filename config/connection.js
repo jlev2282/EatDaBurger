e0fc16d1a4802b6d5e8c3dd303ca018f6695b1c0
John <jlev2282@gmail.com>
2017-09-10 10:12:47 -0400

    created db connection in mysql

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,28 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-// local connection information
 // var connection = mysql.createConnection({
 //   port: 3306,
 //   host: "localhost",
 //   user: "root",
 //   password: "localhost",
 //   database: "burgers_db"
-// });
+// })|| mysql.createConnection(process.env.JAWSDB_URL);
 
-// jaws db connection information
-// var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "localhost",
-  database: "burgers_db"
-})|| mysql.createConnection(process.env.JAWSDB_URL);
+var connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "localhost",
+        database: "burgers_db",
+    });
+};
 
 // Make connection.
 connection.connect(function(err) {
